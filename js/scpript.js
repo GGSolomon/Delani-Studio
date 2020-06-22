9488b474d794a5a3efd31b88bc279122f9725103
GGSolomon <solomonggichuru@gmail.com>
2020-06-22 16:12:01 +0300

    Add script.css content

--- js/scpript.js
+++ js/scpript.js
@@ -1,94 +1,95 @@
 $(document).ready(function () {
-     $(".Picture").hide();
+  $(".Picture").hide();
 
-
-
-        $("#FirstPort").hover(function() {
-          $("#Task4").fadeTo("slow", 0.6);
+        $("#Portal1").hover(function() {
+          $("#work4").fadeTo("slow", 0.6);
           $("#Picture1").fadeTo("slow", 1);
         }, function() {
-          $("#Task4").fadeTo("slow", 1);
+          $("#work4").fadeTo("slow", 1);
           $("#Picture1").fadeTo("slow", 0);
         });
 
-        $("#SecondPort").hover(function() {
-          $("#Task3").fadeTo("slow", 0.6);
+        $("#Portal2").hover(function() {
+          $("#work3").fadeTo("slow", 0.6);
           $("#Picture2").fadeTo("slow", 1);
         }, function() {
-          $("#Task3").fadeTo("slow", 1);
+          $("#work3").fadeTo("slow", 1);
           $("#Picture2").fadeTo("slow", 0);
         });
 
-        $("#ThirdPort").hover(function() {
-          $("#Task2").fadeTo("slow", 0.6);
+        $("#Portal3").hover(function() {
+          $("#work2").fadeTo("slow", 0.6);
           $("#Picture3").fadeTo("slow", 1);
         }, function() {
-          $("#Task2").fadeTo("slow", 1);
+          $("#work2").fadeTo("slow", 1);
           $("#Picture3").fadeTo("slow", 0);
         });
 
-        $("#ForthPort").hover(function() {
-          $("#Task1").fadeTo("slow", 0.6);
+        $("#Portal4").hover(function() {
+          $("#work1").fadeTo("slow", 0.6);
           $("#Picture4").fadeTo("slow", 1);
         }, function() {
-          $("#Task1").fadeTo("slow", 1);
+          $("#work1").fadeTo("slow", 1);
           $("#Picture4").fadeTo("slow", 0);
         });
-        $("#FifthPort").hover(function() {
-          $("#Task5").fadeTo("slow", 0.6);
+
+        $("#Portal5").hover(function() {
+          $("#work5").fadeTo("slow", 0.6);
           $("#Picture5").fadeTo("slow", 1);
         }, function() {
-          $("#Task5").fadeTo("slow", 1);
+          $("#work5").fadeTo("slow", 1);
           $("#Picture5").fadeTo("slow", 0);
         });
-        $("#SixthPort").hover(function() {
-          $("#Task6").fadeTo("slow", 0.6);
+
+        $("#Portal6").hover(function() {
+          $("#work6").fadeTo("slow", 0.6);
           $("#Picture6").fadeTo("slow", 1);
         }, function() {
-          $("#Task6").fadeTo("slow", 1);
+          $("#work6").fadeTo("slow", 1);
           $("#Picture6").fadeTo("slow", 0);
         });
-        $("#SeventhPort").hover(function() {
-          $("#Task7").fadeTo("slow", 0.6);
+
+        $("#Portal7").hover(function() {
+          $("#work7").fadeTo("slow", 0.6);
           $("#Picture7").fadeTo("slow", 1);
         }, function() {
-          $("#Task7").fadeTo("slow", 1);
+          $("#work7").fadeTo("slow", 1);
           $("#Picture7").fadeTo("slow", 0);
         });
-        $("#EighthPort").hover(function() {
-          $("#Task8").fadeTo("slow", 0.6);
+
+        $("#Portal8").hover(function() {
+          $("#work8").fadeTo("slow", 0.6);
           $("#Picture8").fadeTo("slow", 1);
         }, function() {
-          $("#Task8").fadeTo("slow", 1);
+          $("#work8").fadeTo("slow", 1);
           $("#Picture8").fadeTo("slow", 0);
         });
 
 
-
-    $(".click1").click(function () {
-        $("#design").toggle();
-        $("#pdesign").toggle();
+    $(".DesClick").click(function () {
+        $("#Design").toggle();
+        $("#OurDesign").toggle();
     })
 
-    $(".click2").click(function () {
-        $("#development").toggle();
-        $("#pdevelopment").toggle();
+    $(".DevClick").click(function () {
+        $("#Dev").toggle();
+        $("#OurDev").toggle();
     })
-    $(".click3").click(function () {
-        $("#management").toggle();
-        $("#pmanagement").toggle();
+    $(".ProdClick").click(function () {
+        $("#Mgt").toggle();
+        $("#OurMgt").toggle();
     })
 
 
 
     $("#contacts").submit(function (event) {
         event.preventDefault();
-        var nill = ["name", "mail", "message"];
-        var text = [];
-        nill.forEach(function (blank) {
-            text.push($("#" + blank).val());
+        var blanks = ["name", "mail", "message"];
+        var input = [];
+        blanks.forEach(function (blank) {
+            input.push($("#" + blank).val());
         });
-        alert("Thank You " + text[0] + " for contacting us. We have received your Email");
+        alert("Thank You " + input[0] + " for contacting us. We have received your Email");
         $("#contact")[0].reset();
     });
 
